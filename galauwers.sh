d3d0d36c3175db58016e92ad4e0b6d3105d12a9f
deki van sergio <presidentamvan@gmail.com>
2017-04-08 08:42:43 +0700

    Create galauwers.sh

---
+++ galauwers.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+m="\033[1;31m"
+k="\033[1;33m"
+h="\033[1;32m"
+b="\033[1;34m"
+n="\033[1;0m"
+
+	clear
+
+	loadload(){
+clear
+echo -e "mohon tunggu sebentar? \\ ===D"
+sleep 0.4
+clear
+echo -e "mohon tunggu sebentar? | ======D"
+sleep 0.4
+clear
+echo -e "mohon tunggu sebentar? / ========D"
+sleep 0.4
+clear
+echo -e "mohon tunggu sebentar? \\ ==========D"
+sleep 0.4
+clear
+echo -e "mohon tunggu sebentar? | =============D"
+sleep 0.4
+clear
+echo -e "mohon tunggu sebentar? /================D"
+sleep 0.4
+clear
+}
+
+
+echo -e $m"                __                            "$k"                      " 
+echo -e $k"               [  |                           "$b"                      " 
+echo -e $h"  .--./) ,--.   | |  ,--.  __   _  _   _   __ "$h" .---.  _ .--.  .--.  " 
+echo -e $b" / /''\;''_\ :  | | ''_\ :[  | | |[ \ [ \ [  ]"$k"/ /__\\[ '/''\]( ('\])" 
+echo -e $m" \ \._//// | |, | | // | |,| \_/ |,\ \/\ \/ / "$b"| \__., | |     '.'.  " 
+echo -e $b" .',__' \'-;__/[___]\'-;__/'.__.'_/ \__/\__/  "$h" '.__.'[___]   [\__)) " 
+echo -e $h"( ( __))                                      "$k"                      "
+echo ""
+echo -e $h"--------------------------------------------------------------" 
+echo -e $k"|<?> galau sedikit gak apa apa ya kan ?                      |"
+echo -e $k"|"$m"("$n""$b"+"$n""$m")"$n" "$k"k"$n""$h"e"$n""$m"n"$n""$b"a"$n""$k"p"$n""$h"a"$n" "$b"s"$n""$k"e"$n""$m"m"$n""$k"u"$n""$h"a"$n" "$b"r"$n""$h"a"$n""$m"s"$n""$k"a"$n" "$k"i"$n""$b"n"$n""$k"i"$n" "$b"t"$n""$h"i"$n""$k"d"$n""$m"a"$b"k"$n" "$k"b"$n""$m"i"$n""$b"s"$n""$h"a"$n" "$m"k"$n""$k"a"$n""$b"u"$n" "$b"b"$n""$k"a"$n""$b"l"$n""$m"a"$n""$k"s"$n"              "$k"|"
+echo -e $k"|"$m"("$n""$b"+"$n""$m")"$n" "$k"k"$n""$h"e"$n""$m"n"$n""$b"a"$n""$k"p"$n""$h"a"$n" "$b"s"$n""$k"e"$n""$m"m"$n""$k"u"$n""$h"a"$n" "$b"r"$n""$h"a"$n""$m"s"$n""$k"a"$n" "$k"i"$n""$b"n"$n""$k"i"$n" "$b"t"$n""$h"i"$n""$k"d"$n""$m"a"$b"k"$n" "$k"b"$n""$m"i"$n""$b"s"$n""$h"a"$n" "$m"k"$n""$k"a"$n""$b"u"$n" "$b"t"$n""$k"e"$n""$b"r"$n""$m"i"$n""$k"m"$n""$h"a"$n"             "$k"|"
+echo -e $k"|"$m"("$n""$b"+"$n""$m")"$n" "$k"k"$n""$h"e"$n""$m"n"$n""$b"a"$n""$k"p"$n""$h"a"$n" "$b"s"$n""$k"e"$n""$m"m"$n""$k"u"$n""$h"a"$n" "$b"r"$n""$h"a"$n""$m"s"$n""$k"a"$n" "$k"i"$n""$b"n"$n""$k"i"$n" "$b"t"$n""$h"i"$n""$k"d"$n""$m"a"$b"k"$n" "$k"b"$n""$m"i"$n""$b"s"$n""$h"a"$n" "$m"k"$n""$k"a"$n""$b"u"$n" "$b"r"$n""$k"a"$n""$b"s"$n""$m"a"$n""$k"k"$n""$h"a"$n""$k"n"$n"            "$k"|"
+echo -e $k"|"$m"("$n""$b"+"$n""$m")"$n" "$k"s"$n""$h"e"$n""$m"m"$n""$b"o"$n""$k"g"$n""$h"a"$n" "$b"s"$n""$k"u"$n""$m"a"$n""$k"t"$n""$h"u"$n" "$b"s"$n""$h"a"$n""$m"a"$n""$k"t"$n" "$k"k"$n""$b"a"$n""$k"u"$n" "$b"b"$n""$h"i"$n""$k"s"$n""$m"a"$n" "$k"m"$n""$b"e"$n""$m"n"$n""$k"g"$n""$b"e"$n""$h"r"$n""$m"t"$n""$k"i"$n" "$m"s"$n""$k"e"$n""$h"m"$n""$b"u"$n""$h"a"$n" "$b"r"$n""$k"a"$n""$m"s"$n""$b"a"$n" "$h"i"$n"""$k"$"$n""$n""$b"""$n""$h"n"$n""$n""$h"i"$n"      "$k"|"
+echo -e $h"--------------------------------------------------------------"
+echo ""
+echo ""
+echo -e $k"(=)"$n"      "$m"p"$n""$k"i"$n""$b"l"$n""$k"i"$n""$k"h"$n" "$h"m"$n""$m"a"$n""$k"s"$n" "$b"y"$n""$m"a"$n""$b"n"$n""$k"g"$n" "$b"g"$n""$k"a"$n""$b"n"$n""$m"s"$n""$k"z"$n" "$m"d"$n""$h"a"$n""$k"n"$n" "$b"m"$n""$m"b"$n""$k"a"$n""$h"k"$n" "$k"y"$n""$b"a"$n""$h"n"$n""$m"g"$n" "$b"c"$n""$k"a"$n""$h"n"$n""$m"s"$n""$b"s"$n""$h"z"$n"         "$k"(=)"
+echo ""
+echo -e "       C0d3 bY :>      DekiTr"$k"@"$b"smkn2kalianda$n         "
+echo -e $k"       C0d3 bY :>     Rizky kurnia"$k"@"$m"smksaktigemolong$n "
+echo -e $k"=============================================================="
+echo -e $k"++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo -e $k"[>][1] UpDatE yOuR pEsE"
+echo -e $h"[>][2] Matiin Pe-Se kalian"
+echo -e $k"[>][3] Reboot Pe-Se kalian"
+echo -e $h"[>][4] Melihat Interface"
+echo ""
+echo -e -n $b"Pilih mau yang mana..."$n""$m"@"$n""$h"dekitririzmawan:"
+
+read master;
+
+#update
+if [ $master == "1" ]; then
+	clear
+	loadload
+	echo "Bersiap-siap..."
+	sleep 1
+	sudo apt-get update
+	sleep 1
+	clear
+	./naseb.sh
+fi
+#shutdown
+if [ $master == "2" ]; then
+	clear
+	echo "Mematikan laptop mu jo!"
+	sleep 1
+	clear
+	sudo shutdown -y
+fi
+#reboot
+if [ $master == "3" ]; then
+	clear
+	echo "me-Reboot laptop mu jo!"
+	sleep 1
+	clear
+	sudo reboot
+fi
+#ifconfig
+if [ $master == "4" ]; then
+	clear
+	loadload
+	echo "Menampilkan"
+	sleep 1
+	clear
+	sudo ifconfig
+	sleep 1
+fi
